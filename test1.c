64f1f3490572fe91169522d5e704211d72f88108
Mauro Stettler <mauro.stettler@gmail.com>
2010-02-14 00:21:59 +0800

    added tests

---
+++ test1.c
@@ -0,0 +1,23 @@
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+#include <stdio.h>
+#include "testdata.h"
+#include "parser.c"
+
+
+int main (void)
+{
+    ngx_str_t result;
+    char *result_string;
+    
+    if (extract_value(&test_searchkey, &test_session, &result) == NGX_OK)
+    {
+        result_string = malloc(sizeof(char) * result.len);
+        snprintf(result_string, result.len + 1, "%s", result.data);
+        printf("result: '%s'\n", result_string);
+    } else {
+        printf("no result\n");
+    }
+}
