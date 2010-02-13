8009c8b14e1a5a005657a10395b4c94c835d9045
Mauro Stettler <mauro.stettler@gmail.com>
2010-02-14 01:14:59 +0800

    compilation of tests via compile.sh works

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc -g -ggdb -I. -Isrc/core -Isrc/event -Isrc/event/modules -Isrc/os/unix -Isrc/http -Isrc/http/modules -Isrc/mail -I../nginx-0.7.65/objs -I/Users/maurostettler/Downloads/unpack/pcre-8.00 test1.c
+gcc -g -ggdb -I. -Isrc/core -Isrc/event -Isrc/event/modules -Isrc/os/unix -Isrc/http -Isrc/http/modules -Isrc/mail -Iobjs -I/Users/maurostettler/Downloads/unpack/pcre-8.00 test1.c
